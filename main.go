b410ad72c091beabb2a03a07ded92c5924a4f555
AMCodeBytes <57409463+AMCodeBytes@users.noreply.github.com>
2024-03-01 13:58:17 +0000

    Created ability to play against the computer

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"os"
 )
 
@@ -11,14 +12,14 @@ func main() {
 	fmt.Println("-----   Rock, Paper, Scissors   -----")
 
 	for {
-		fmt.Println("1. Play")
-		fmt.Println("2. Quit")
+		displayMenu()
+
 		fmt.Print("Your choice: ")
 		fmt.Scan(&choice)
 
 		switch choice {
 		case 1:
-			// code
+			play()
 		case 2:
 			os.Exit(0)
 		default:
@@ -27,4 +28,69 @@ func main() {
 
 		choice = 0
 	}
+}
+
+func play() {
+	var playerChoice int
+	var playerAction string
+	var computerChoice int
+	var computerAction string
+	var winner string = ""
+
+	fmt.Println("\n\n-----   Your Move   -----")
+	fmt.Println("1. Rock")
+	fmt.Println("2. Paper")
+	fmt.Println("3. Scissors")
+	fmt.Print("Move: ")
+	fmt.Scan(&playerChoice)
+
+	switch playerChoice {
+	case 1:
+		playerAction = "Rock"
+	case 2:
+		playerAction = "Paper"
+	case 3:
+		playerAction = "Scissors"
+	default:
+		panic("Invalid input entered")
+	}
+
+	computerChoice = rand.Intn(3) + 1
+
+	playerAction = getAction(playerChoice)
+	computerAction = getAction(computerChoice)
+
+	fmt.Printf("Player - %v vs %v - Computer\n", playerAction, computerAction)
+
+	if playerChoice == 1 && computerChoice == 3 {
+		winner = "Winner!"
+	} else if playerChoice == 2 && computerChoice == 1 {
+		winner = "Winner!"
+	} else if playerChoice == 3 && computerChoice == 2 {
+		winner = "Winner!"
+	} else if playerChoice == computerChoice {
+		winner = "Draw!"
+	} else {
+		winner = "Loser!"
+	}
+
+	fmt.Printf("\n\n----- %v -----\n\n", winner)
+}
+
+func getAction(chose int) string {
+	switch chose {
+	case 1:
+		return "Rock"
+	case 2:
+		return "Paper"
+	case 3:
+		return "Scissors"
+	default:
+		panic("Invalid input entered")
+	}
+}
+
+func displayMenu() {
+	fmt.Println("1. Play")
+	fmt.Println("2. Quit")
 }
